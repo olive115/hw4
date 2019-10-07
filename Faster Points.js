ec9f74c9a9a88b89f58ac2f3405995b96bdb132f
olive115 <55379920+olive115@users.noreply.github.com>
2019-10-07 11:30:06 -0700

    Create Faster Points.js

---
+++ Faster Points.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB, width, height, 100);
+}
+
+function draw() {
+  var x = random(width);
+  var y = random(height);
+  
+  for (i=0; i<101; i++){
+    stroke(x, y, 100);
+    point(x, y);
+    print ("point!");
+}
+}
